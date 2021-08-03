1b159b7e0fab4bd1605b057036bc2688c4ed856d
Michał Piszczek <michal@piszczek.pl>
2021-08-03 21:40:45 +0200

    data validations fix

--- src/PhpSpreadsheet/Reader/Xlsx.php
+++ src/PhpSpreadsheet/Reader/Xlsx.php
@@ -390,6 +390,7 @@ class Xlsx extends BaseReader
 
         // Initialisations
         $excel = new Spreadsheet();
+
         $excel->removeSheetByIndex(0);
         if (!$this->readDataOnly) {
             $excel->removeCellStyleXfByIndex(0); // remove the default style
@@ -1265,6 +1266,24 @@ class Xlsx extends BaseReader
                                     if ($brk['man']) {
                                         $docSheet->setBreak(Coordinate::stringFromColumnIndex((string) $brk['id'] + 1) . '1', Worksheet::BREAK_COLUMN);
                                     }
+                                }
+                            }
+
+                            // handle Microsoft extension if present
+                            if (isset ($xmlSheet->extLst, $xmlSheet->extLst->ext, $xmlSheet->extLst->ext['uri'])
+                                && $xmlSheet->extLst->ext['uri'] == "{CCE6A557-97BC-4b89-ADB6-D9C93CAAB3DF}" )
+                            {
+                                if( ! $xmlSheet->dataValidations ){
+                                    $xmlSheet->addChild('dataValidations');
+                                }
+                                // retreive MS extension data to create a node that matches expectations.
+                                foreach ($xmlSheet->extLst->ext->children('x14', TRUE)->dataValidations->dataValidation as $item)
+                                {
+                                    $node = $xmlSheet->dataValidations->addChild('dataValidation');
+                                    foreach ($item->attributes() as $attr)
+                                        $node->addAttribute($attr->getName(), $attr);
+                                    $node->addAttribute('sqref', $item->children('xm',TRUE)->sqref);
+                                    $node->addChild('formula1', $item->formula1->children('xm',TRUE)->f);
                                 }
                             }
 
